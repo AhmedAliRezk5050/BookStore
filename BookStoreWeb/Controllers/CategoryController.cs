a7ec6049c593496a0c2941cd30985c0c999ceae1
ahmedalirezk5050 <ahmedalirezk5050@gmail.com>
2022-08-18 05:03:53 +0200

    implement(category create action

--- BookStoreWeb/Controllers/CategoryController.cs
+++ BookStoreWeb/Controllers/CategoryController.cs
@@ -1,4 +1,5 @@
 ﻿using BookStoreWeb.Data;
+using BookStoreWeb.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -6,19 +7,49 @@ namespace BookStoreWeb.Controllers
 {
     public class CategoryController : Controller
     {
-
         private readonly DataContext _context;
+        private readonly ILogger<CategoryController> _logger;
 
-        public CategoryController(DataContext context)
+        public CategoryController(DataContext context, ILogger<CategoryController> logger)
         {
             _context = context;
+            _logger = logger;
         }
 
         public async Task<IActionResult> Index()
         {
-            var categories = await _context.Categories.ToListAsync();
+            var categories = await _context.Categories.AsNoTracking().ToListAsync();
 
             return View(categories);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Name", "DisplayOrder")] Category category)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    _context.Add(category);
+                    await _context.SaveChangesAsync();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (DbUpdateException ex)
+            {
+                _logger.LogError(ex, "An error occurred while creating the Category.");
+                ModelState.AddModelError("", "Unable to save changes. " +
+                                             "Try again, and if the problem persists " +
+                                             "see your system administrator.");
+            }
+
+            return View(category);
         }
     }
 }
--- BookStoreWeb/Program.cs
+++ BookStoreWeb/Program.cs
@@ -5,70 +5,69 @@ using Microsoft.Extensions.Hosting;
 namespace BookStoreWeb;
 public class Program
 {
-    public static void Main(string[] args)
+  public static void Main(string[] args)
+  {
+    var builder = WebApplication.CreateBuilder(args);
+
+    builder.Services.AddDbContext<DataContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("BookStoreConnection")));
+
+    builder.Services.AddControllersWithViews();
+
+    if (builder.Environment.IsDevelopment())
     {
-        var builder = WebApplication.CreateBuilder(args);
-
-        builder.Services.AddDbContext<DataContext>(options =>
-        options.UseSqlServer(builder.Configuration.GetConnectionString("BookStoreConnection")));
-
-        builder.Services.AddControllersWithViews();
-
-        if (builder.Environment.IsDevelopment())
-        {
-            builder.Services.AddDatabaseDeveloperPageExceptionFilter();
-        }
-
-        var app = builder.Build();
-
-        if (app.Environment.IsDevelopment())
-        {
-            app.UseDeveloperExceptionPage();
-        }
-        else
-        {
-            app.UseExceptionHandler("/Home/Error");
-            app.UseHsts();
-        }
-
-        app.UseStaticFiles();
-
-        app.UseRouting();
-
-        app.UseAuthorization();
-
-        app.MapControllerRoute(
-            name: "default",
-            pattern: "{controller=Home}/{action=Index}/{id?}");
-
-        SeedDb(app);
-         
-
-        app.Run();
+      builder.Services.AddDatabaseDeveloperPageExceptionFilter();
     }
 
-    private static void SeedDb(WebApplication app)
+    var app = builder.Build();
+
+    if (app.Environment.IsDevelopment())
     {
-        using (var scope = app.Services.CreateScope())
-        {
-            var services = scope.ServiceProvider;
-
-            try
-            {
-                var context = services.GetRequiredService<DataContext>();
-
-                SeedData.SeedDatabase(context);
-            }
-            catch (Exception ex)
-            {
-                var logger = services.GetRequiredService<ILogger<Program>>();
-
-                logger.LogError(ex, "An error occurred while seeding the database.");
-            }
-        }
+      app.UseDeveloperExceptionPage();
     }
+    else
+    {
+      app.UseExceptionHandler("/Home/Error");
+      app.UseHsts();
+    }
+
+    app.UseStaticFiles();
+
+    app.UseRouting();
+
+    app.UseAuthorization();
+
+    app.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Home}/{action=Index}/{id?}");
+
+    SeedDb(app);
+
+    app.Run();
+  }
+
+  private static void SeedDb(WebApplication app)
+  {
+    using (var scope = app.Services.CreateScope())
+    {
+      var services = scope.ServiceProvider;
+
+      try
+      {
+        var context = services.GetRequiredService<DataContext>();
+
+        SeedData.SeedDatabase(context);
+      }
+      catch (Exception ex)
+      {
+        var logger = services.GetRequiredService<ILogger<Program>>();
+
+        logger.LogError(ex, "An error occurred while seeding the database.");
+      }
+    }
+  }
 }
 
 
- 
+
 
