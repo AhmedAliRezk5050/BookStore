135147712cc104c0dd502c21e2774ec2ff84f079
ahmedalirezk5050 <ahmedalirezk5050@gmail.com>
2022-08-18 09:59:38 +0200

    implement(category delete action)

--- BookStoreWeb/Controllers/CategoryController.cs
+++ BookStoreWeb/Controllers/CategoryController.cs
@@ -73,7 +73,7 @@ namespace BookStoreWeb.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> EditPost(int? id)
         {
-            if (IsValidId(id))
+            if (!IsValidId(id))
             {
                 return NotFound();
             }
@@ -97,20 +97,69 @@ namespace BookStoreWeb.Controllers
                     await _context.SaveChangesAsync();
                     return RedirectToAction(nameof(Index));
                 }
-                catch (DbUpdateException  ex)
+                catch (DbUpdateException ex)
                 {
                     _logger.LogError(ex, "An error occurred while updating the Category.");
-                    
+
                     ModelState.AddModelError("", "Unable to save changes. " +
                                                  "Try again, and if the problem persists, " +
                                                  "see your system administrator.");
                 }
             }
-            
+
             return View(categoryToUpdate);
         }
 
-        private static bool IsValidId(int? id) => id is null or 0;
+        public async Task<IActionResult> Delete(int? id, bool? saveChangesError)
+        {
+            if (!IsValidId(id))
+            {
+                return NotFound();
+            }
+
+            var category = await _context.Categories.FirstOrDefaultAsync(c => c.Id == id);
+
+            if (!IsCategoryExist(category))
+            {
+                return NotFound();
+            }
+
+            if (saveChangesError.GetValueOrDefault())
+            {
+                ViewData["ErrorMessage"] =
+                    "Delete failed. Try again, and if the problem persists " +
+                    "see your system administrator.";
+            }
+
+            return View(category!);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int? id)
+        {
+            var category = await _context.Categories.FindAsync(id);
+
+            if (!IsCategoryExist(category))
+            {
+                return RedirectToAction(nameof(Index));
+            }
+
+            try
+            {
+                _context.Categories.Remove(category!);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "An error occurred while deleting the Category.");
+                return RedirectToAction(nameof(Delete), new { id = id, saveChangesError = true });
+            }
+        }
+
+
+        private static bool IsValidId(int? id) => id is not null or 0;
 
         private static bool IsCategoryExist(Category? category) => category != null;
     }
