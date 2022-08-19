5dfc8401c045483515a753d1e781de1d69d1f465
ahmedalirezk5050 <ahmedalirezk5050@gmail.com>
2022-08-19 06:20:37 +0200

    razor pages - add(Create) page

---
+++ BookStoreWeb/Pages/Categories/Create.cshtml.cs
@@ -0,0 +1,50 @@
+﻿using BookStoreWeb.Data;
+using BookStoreWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BookStoreWeb.Pages.Categories;
+
+public class CreateModel : PageModel
+{
+    private readonly DataContext _context;
+
+    public CreateModel(DataContext context)
+    {
+        _context = context;
+    }
+
+    [BindProperty] public Category Category { get; set; } = null!;
+    
+    public IActionResult OnGet()
+    {
+        return Page();
+    }
+
+    public async Task<IActionResult> OnPostAsync()
+    {
+        if (!ModelState.IsValid)
+        {
+            return Page();
+        }
+
+        var emptyCategory = new Category();
+
+        if (await TryUpdateModelAsync<Category>(
+                emptyCategory,
+                "category",
+                c => c.Name,
+                c => c.DisplayOrder!
+            )
+           )
+        {
+            _context.Categories.Add(emptyCategory);
+
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+
+        return new EmptyResult();
+    }
+}
