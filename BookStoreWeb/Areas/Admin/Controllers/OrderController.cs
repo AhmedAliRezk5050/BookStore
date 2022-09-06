26c0a90444b5e6813942f298753d8b6f18840ad3
ahmedalirezk5050 <ahmedalirezk5050@gmail.com>
2022-09-06 09:22:33 +0200

    orders management(Role based order display)

--- BookStoreWeb/Areas/Admin/Controllers/OrderController.cs
+++ BookStoreWeb/Areas/Admin/Controllers/OrderController.cs
@@ -1,10 +1,15 @@
 ﻿using System.Collections;
+using System.Security.Claims;
 using BookStore.DataAccess.Repository.IRepository;
+using BookStore.Models;
+using BookStore.Utility;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BookStoreWeb.Areas.Admin.Controllers;
 
 [Area("Admin")]
+[Authorize]
 public class OrderController : Controller
 {
     private readonly IUnitOfWork _unitOfWork;
@@ -13,17 +18,32 @@ public class OrderController : Controller
     {
         _unitOfWork = unitOfWork;
     }
-    
+
     public IActionResult Index(string? status)
     {
         return View();
     }
 
     #region API
+
     public async Task<IActionResult> GetAll()
     {
-        var orders = await _unitOfWork.OrderRepository.GetAllAsync(includedProperties: "OrderDetails,ApplicationUser");
+        List<Order> orders;
+        if (User.IsInRole(SD.AdminRole) || User.IsInRole(SD.EmployeeRole))
+        {
+            orders = await _unitOfWork.OrderRepository.GetAllAsync(includedProperties: "ApplicationUser");
+        }
+        else
+        {
+            var userClaim = User.FindFirst(ClaimTypes.NameIdentifier);
+            var currentUserId = userClaim!.Value;
+            orders = await _unitOfWork.OrderRepository.
+                GetAllAsync(o => o.ApplicationUserId == currentUserId, includedProperties: "ApplicationUser");
+        }
+
+
         return Json(orders);
     }
+
     #endregion
 }
